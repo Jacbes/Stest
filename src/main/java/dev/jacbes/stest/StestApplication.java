81e534f16d51cc51fcfb3bc71c6faec1f1cc002a
Jacbes <skishon100000@gmail.com>
2022-11-07 17:43:32 +0700

    Update .gitignore

--- src/main/java/dev/jacbes/stest/StestApplication.java
+++ src/main/java/dev/jacbes/stest/StestApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StestApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(StestApplication.class, args);
+    }
 
 }
